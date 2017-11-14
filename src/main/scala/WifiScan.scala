0c4913f2b1d50090b4f4f9c3da6fcca20eba95e1
felix.johnsen@linkmobility.com <felix.johnsen@linkmobility.com>
2017-11-14 12:56:32 +0100

    bump

--- src/main/scala/WifiScan.scala
+++ src/main/scala/WifiScan.scala
@@ -4,8 +4,12 @@ import sys.process._
 
 object WifiScan extends App {
   val scanCmd = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s"
+
   case class SsidProbe(ssid: String, bssid: String, strengthDb: Int, channelBase: Int, channelExtend: String)
 
   val scanResult = WifiScan.scanCmd.!!
-  println(scanResult)
+  //println(scanResult)
+  val lines = scanResult.split("\n")
+  val someScan = lines(2).split(" +") //split multi whitespace
+
 }
